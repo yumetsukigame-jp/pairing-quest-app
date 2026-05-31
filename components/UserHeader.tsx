357b8d8b0b0098b74bd2d43b4b7d10387e641fe5
yumetsuki <yumetsukigame@gmail.com>
2026-05-31 15:35:03 +0900

    update

--- components/UserHeader.tsx
+++ components/UserHeader.tsx
@@ -1,15 +1,19 @@
 "use client";
 
 import Link from "next/link";
+import Image from "next/image";
 
 export default function UserHeader() {
   return (
     <header className="w-full">
-      <Link href="/">
-        <img
+      <Link href="/" className="block">
+        <Image
           src="/header.webp"
           alt="ゆめつきの書斎 ヘッダー"
-          className="w-full h-auto object-contain cursor-pointer"
+          width={1200}
+          height={300}
+          className="w-full h-auto object-contain cursor-pointer block"
+          priority
         />
       </Link>
     </header>
